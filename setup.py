d78c085117e9853863e56d9fe7ced9b30c86e0fb
TeamSingaSushi <70812649+TeamSingaSushi@users.noreply.github.com>
2020-09-07 15:28:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-setup(
-    dependency_links=[
-        "https://github.com/OSGeo/GDAL.git",
-        "https://github.com/Toblerity/Fiona.git",
-        "https://github.com/Toblerity/Shapely.git",
-        "https://github.com/pyproj4/pyproj.git",
-        "https://github.com/Toblerity/Rtree.git"
-    ]
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TeamSingaSushi", # Replace with your own username
+    version="3.1.1",
+    description="GDAL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TeamSingaSushi/gdal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.2',
 )
